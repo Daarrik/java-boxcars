9132b24c6455c872a3fd837aad5178c2b54430cd
Daarrik <darrikwh@gmail.com>
2022-02-05 00:55:50 -0800

    formatting

--- Boxcars.java
+++ Boxcars.java
@@ -1,122 +1,124 @@
 import java.util.Scanner;
 
 public class Boxcars {
-    
-    private PairOfDice dice;
-    int computerScore;
-    int humanScore;
-    String humanName;
 
-    public Boxcars2() {
-        // Initialization of instance variables happens in method init
-    }
+  private PairOfDice dice;
+  int computerScore;
+  int humanScore;
+  String humanName;
 
-    public void init(String name) {
-        dice = new PairOfDice();
-        computerScore = 0;
-        humanScore = 0;
-        humanName = name;
-    }
+  public Boxcars() {
+    // Initialization of instance variables happens in method init
+  }
 
-    public void playGame() {
-        boolean finished = false;
+  public void init(String name) {
+    dice = new PairOfDice();
+    computerScore = 0;
+    humanScore = 0;
+    humanName = name;
+  }
 
-        while(!finished) {
-            
-            // Computer turn
-            turn(true);
-            printScore();
-            pressEnter();
+  public void playGame() {
+    boolean finished = false;
 
-            // Human turn
-            turn(false);
-            printScore();
-            pressEnter();
+    while (!finished) {
 
-            // Check if there is a winner after both turns have been played.
-            if(humanScore >= 150 || computerScore >= 150) {
-                if(humanScore > computerScore) {
-                    System.out.println(humanName + ", Congratulations! You beat the computer!");
-                    finished = true;
-                } else {
-                    System.out.println("Sorry, " + humanName + " you got beat by the computer!");
-                    finished = true;
-                }
-            }
+      // Computer turn
+      turn(true);
+      printScore();
+      pressEnter();
+
+      // Human turn
+      turn(false);
+      printScore();
+      pressEnter();
+
+      // Check if there is a winner after both turns have been played.
+      if (humanScore >= 150 || computerScore >= 150) {
+        if (humanScore > computerScore) {
+          System.out.println(humanName + ", Congratulations! You beat the computer!");
+          finished = true;
+        } else {
+          System.out.println("Sorry, " + humanName + " you got beat by the computer!");
+          finished = true;
         }
+      }
     }
+  }
 
-    public void turn(boolean computer) {
-        int turnScore = 0;
-        int numOfRolls = 0;
-        int multiplier = 1;
-        boolean doubles = false;
-        Scanner kb = new Scanner(System.in);
-        String play = "y";
+  public void turn(boolean computer) {
+    int turnScore = 0;
+    int numOfRolls = 0;
+    int multiplier = 1;
+    boolean doubles = false;
+    Scanner kb = new Scanner(System.in);
+    String play = "y";
 
-        // If computer is true, print out "Computer's turn: ", otherwise printout humanName + "'s turn:'"
-        if(computer)
-            System.out.println("Computer's turn:");
-        else
-            System.out.println(humanName + "'s turn:");
+    // If computer is true, print out "Computer's turn: ", otherwise printout
+    // humanName + "'s turn:'"
+    if (computer)
+      System.out.println("Computer's turn:");
+    else
+      System.out.println(humanName + "'s turn:");
 
-        while( ((computer && turnScore < 20) || (!computer && play.equals("y"))) && !doubles && numOfRolls < 5) {
-            dice.roll();
-            System.out.println("\tRolled: " + dice.toString());
-            if(dice.getDie1() == dice.getDie2()) {
-                if(dice.getDie1() == 1) {
-                    System.out.println("\tRolled snake eyes! All turn points will be doubled.");
-                    turnScore  += 2;
-                    multiplier *= 2;
-                } else if (dice.getDie1() == 6) {
-                    System.out.println("\tRolled box cars! All points are gone now!");
-                    turnScore = 0;
-                    multiplier = 1;
-                    if(computer)
-                        computerScore = 0;
-                    else
-                        humanScore = 0;
-                    doubles = true;
-                } else {
-                    System.out.println("\t\tRolled double. . . lose all turn points.");
-                    turnScore = 0;
-                    multiplier = 1;
-                    doubles = true;
-                }
-            } else {
-                turnScore += dice.getDie1() + dice.getDie2();
-            }
-            numOfRolls++;
-            if(!doubles)
-                System.out.println("\tCurrent score for this turn: " + turnScore);
-
-            if(!doubles && numOfRolls < 5 && !computer) {
-                System.out.print("Roll again? (y/n) ");
-                play = kb.next().toLowerCase();
-            } else {
-                play = "n";
-            }
+    while (((computer && turnScore < 20) || (!computer && play.equals("y"))) && !doubles && numOfRolls < 5) {
+      dice.roll();
+      System.out.println("\tRolled: " + dice.toString());
+      if (dice.getDie1() == dice.getDie2()) {
+        if (dice.getDie1() == 1) {
+          System.out.println("\tRolled snake eyes! All turn points will be doubled.");
+          turnScore += 2;
+          multiplier *= 2;
+        } else if (dice.getDie1() == 6) {
+          System.out.println("\tRolled box cars! All points are gone now!");
+          turnScore = 0;
+          multiplier = 1;
+          if (computer)
+            computerScore = 0;
+          else
+            humanScore = 0;
+          doubles = true;
+        } else {
+          System.out.println("\t\tRolled double. . . lose all turn points.");
+          turnScore = 0;
+          multiplier = 1;
+          doubles = true;
         }
-        
-        if(multiplier != 1) {
-            System.out.println("\t\tSnake Eyes cause " + turnScore + " points to become " + turnScore * multiplier + " points!");
-            turnScore *= multiplier;
-        }
+      } else {
+        turnScore += dice.getDie1() + dice.getDie2();
+      }
+      numOfRolls++;
+      if (!doubles)
+        System.out.println("\tCurrent score for this turn: " + turnScore);
 
-        if(computer)
-            computerScore += turnScore;
-        else
-            humanScore += turnScore;
+      if (!doubles && numOfRolls < 5 && !computer) {
+        System.out.print("Roll again? (y/n) ");
+        play = kb.next().toLowerCase();
+      } else {
+        play = "n";
+      }
     }
 
-    public void printScore() {
-        System.out.println("CURRENT GAME SCORE: Computer: " + computerScore + "      " + humanName + ": " + humanScore);
+    if (multiplier != 1) {
+      System.out
+          .println("\t\tSnake Eyes cause " + turnScore + " points to become " + turnScore * multiplier + " points!");
+      turnScore *= multiplier;
     }
 
-    public void pressEnter() {
-        Scanner kb = new Scanner(System.in);
-        System.out.println();
-        System.out.print("Press ENTER to continue ...");
-        kb.nextLine();
-    }
+    if (computer)
+      computerScore += turnScore;
+    else
+      humanScore += turnScore;
+  }
+
+  public void printScore() {
+    System.out.println("CURRENT GAME SCORE: Computer: " + computerScore + "      " + humanName + ": " + humanScore);
+  }
+
+  public void pressEnter() {
+    Scanner kb = new Scanner(System.in);
+    System.out.println();
+    System.out.print("Press ENTER to continue ...");
+    kb.nextLine();
+  }
 }
--- Die.java
+++ Die.java
@@ -2,24 +2,24 @@ import java.util.Random;
 
 public class Die {
 
-    public static Random rand = new Random();
-    public static final int NUMBER_OF_SIDES = 6;
-    private int face;
+  public static Random rand = new Random();
+  public static final int NUMBER_OF_SIDES = 6;
+  private int face;
 
-    public Die() {
-        face = 0;
-    }
+  public Die() {
+    face = 0;
+  }
 
-    public int roll() {
-        face = rand.nextInt(NUMBER_OF_SIDES) + 1;
-        return face;
-    }
+  public int roll() {
+    face = rand.nextInt(NUMBER_OF_SIDES) + 1;
+    return face;
+  }
 
-    public int getFace() {
-        return face;
-    }
+  public int getFace() {
+    return face;
+  }
 
-    public String toString() {
-        return "" + face;
-    }
+  public String toString() {
+    return "" + face;
+  }
 }
